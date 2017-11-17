32b7f578cbc94600afeb4e06c4c231ff0bf60eb8
Pzda- <laurengocs.1@gmail.com>
2017-11-17 15:52:51 +0800

    added repo

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loader : MonoBehaviour {
+
+	public GameObject gameManager;
+
+	/// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	void Awake()
+	{
+		if (GameManager.instance == null)
+			Instantiate(gameManager);
+	}
+}
